72847515c05856d5a9dfb65199d5b6574de26249
Gosha <goshatechnique@gmail.com>
2020-01-20 18:18:07 +0300

    Fix web-version, re-css images

---
+++ src/components/ArticleLast.js
@@ -0,0 +1,58 @@
+import React from "react";
+import "../styles/article-last.scss";
+import twitterIcon from "../images/twitterIcon.png";
+import facebookIcon from "../images/facebookIcon.png";
+import googleIcon from "../images/googleIcon.png";
+import vkIcon from "../images/vkIcon.png";
+
+const ArticleLast = () => {
+  return (
+    <div className="article-last">
+      <div className="article-last-information">
+        <div className="information-left">
+          <div>
+            <strong>SOME</strong>APPLICATION
+            <p className="inf-left-p">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Doloribus necessitatibus reiciendis, culpa eaque sed magnam
+              temporibus similique. Aliquam dolorum officiis iste aliquid
+              impedit reprehenderit! Vitae, sapiente sequi. Nemo, corporis
+              dicta.
+            </p>
+            <div className="inf-left-social">
+              <img className="social-img" src={twitterIcon} alt="#"></img>
+              <img className="social-img" src={googleIcon} alt="#"></img>
+              <img className="social-img" src={vkIcon} alt="#"></img>
+              <img className="social-img" src={facebookIcon} alt="#"></img>
+            </div>
+          </div>
+        </div>
+        <div className="information-right">
+          <div className="inf-right-column">
+            <h6 style={{ margin: "0px" }}>LEARN MORE</h6>
+            <p className="inf-right-p">How it works?</p>
+            <p className="inf-right-p">Meeting tools</p>
+            <p className="inf-right-p">Live striming</p>
+            <p className="inf-right-p">Contact method</p>
+          </div>
+          <div className="inf-right-column">
+            <h6 style={{ margin: "0px" }}>ABOUT US</h6>
+            <p className="inf-right-p">About us</p>
+            <p className="inf-right-p">Features</p>
+            <p className="inf-right-p">Privacy police</p>
+            <p className="inf-right-p">Terms & Conditions</p>
+          </div>
+          <div className="inf-right-column">
+            <h6 style={{ margin: "0px" }}>SUPPORT</h6>
+            <p className="inf-right-p">F.A.Q.</p>
+            <p className="inf-right-p">Contact us</p>
+            <p className="inf-right-p">Live chat</p>
+            <p className="inf-right-p">Phone call</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ArticleLast;
